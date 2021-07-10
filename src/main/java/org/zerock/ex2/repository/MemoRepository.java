4d332411902d2b526c7e5ec99a2309207b33be25
Eunsunny96 <85103972+Eunsunny96@users.noreply.github.com>
2021-07-11 00:00:56 +0900

    jpa2

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,7 +1,18 @@
 package org.zerock.ex2.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo,Long> {//<Entity type의 정보, @Id 타입> 인터페이스 선언으로도 자동으로 스프링 bean으로 등록
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);//메서드 이름이 길어지고 혼동하기 쉬움
+
+
+    Page<Memo> findByMnoBetween(Long from, Long to , Pageable pageable); //쿼리 메서드는 pageable결합해서 사용 가능
+
+
 }
--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -6,9 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -21,10 +23,10 @@ class MemoRepositoryTest {
     MemoRepository memoRepository;
 
     @Test
-    void testInsertDummis(){//100개의 새로운 memo 객체 생성, memoRepository를 이용해 inesrt
+    void testInsertDummis() {//100개의 새로운 memo 객체 생성, memoRepository를 이용해 inesrt
 
-        IntStream.rangeClosed(1,100).forEach(i ->{
-            Memo memo =Memo.builder().menoText("sample"+i).build();//text에는 not null 조건임
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().menoText("sample" + i).build();//text에는 not null 조건임
             memoRepository.save(memo);
         });
 
@@ -32,21 +34,22 @@ class MemoRepositoryTest {
     }
 
     @Test
-    void testSelect(){
-        Long mno =100L;
-        Optional<Memo> result=memoRepository.findById(mno);//Optional 타입으로 반환됨
+    void testSelect() {
+        Long mno = 100L;
+        Optional<Memo> result = memoRepository.findById(mno);//Optional 타입으로 반환됨
 
         System.out.println("=====================");//findById()를 실행한 순한 sql 처리
 
-        if(result.isPresent()){
+        if (result.isPresent()) {
             Memo memo = result.get();
             System.out.println(memo);
         }
 
     }
+
     @Transactional
     @Test
-    void testSelect2(){
+    void testSelect2() {
         Long mno = 100L;
         Memo memo = memoRepository.getOne(mno); //getOne() 은 실제 객체가 필요한 순간까지 sql을 실행하지 않음
 
@@ -55,7 +58,7 @@ class MemoRepositoryTest {
     }
 
     @Test
-    void testUpdate(){//수정 작업
+    void testUpdate() {//수정 작업
 
         Memo memo = Memo.builder().mno(100L).menoText("update Text").build();//selct로 해당 번호를  확인한 후 update
         //해당 @Id를 가진 객체가 있다면 update 없다면 insert실행한다
@@ -64,7 +67,7 @@ class MemoRepositoryTest {
     }
 
     @Test
-    void testDelete(){//삭제
+    void testDelete() {//삭제
         Long mno = 100L;//삭제 하려는 번호
         memoRepository.deleteById(mno);//객체가 있는지 확인후 삭제
         //만일 데이터가 존재하지 않으면 예외 발생(에러남)
@@ -72,9 +75,64 @@ class MemoRepositoryTest {
     }
 
     @Test
-    void testPageDefault(){
-        Pageable pageable= PageRequest.of(0,10);//1페이지에 10개 ,페이지는 0부터 시작함
-        Page<Memo> result =memoRepository.findAll(pageable);
-        System.out.println(result);
+    void testPageDefault() {
+        Pageable pageable = PageRequest.of(0, 10);//1페이지에 10개 ,페이지는 0부터 시작함
+
+        Page<Memo> result = memoRepository.findAll(pageable);//목록만 가져오는게 아니고,실제 페이지에 필요한 전체 데이터의 갯수도 가져옴
+
+        System.out.println(result);//첫번째 쿼리 : limit구문 사용해 페이징 처리, 두번째 쿼리 count를 이용해 전체 갯수 처리 ,returntype을 Page<엔티티 타입>지정
+
+        System.out.println("================"); //Pagd<엔티티 타입>을 이용해서 주로 사용하는 메서드들
+
+        System.out.println("Total Pages : " + result.getTotalPages());// 총 몇페이지
+
+        System.out.println("Total count : " + result.getTotalElements()); //전체 갯수
+
+        System.out.println("Page Number : " + result.getNumber()); //현제 페이지 번호 (0부터 시작)
+
+        System.out.println("Page Size : " + result.getSize()); //페이지당 데이터 갯수
+
+        System.out.println("has next page? : " + result.hasNext()); //다음 페이지 존재 여부
+
+        System.out.println("fist page : " + result.isFirst()); //시작 페이지(0) 여부
+
+        System.out.println("========================");
+        for (Memo memo : result.getContent()) { //List<엔티티 타입>으로 처리하거나 Stream <엔티티 타입>을 반환하는 get을 이용할수 있음
+            System.out.println(memo);
+        }
+
+
     }
+
+    @Test
+    void testSort() {//정렬
+        Sort sort1 = Sort.by("mno").descending();//mno를 역순으로 정렬
+
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+        result.get().forEach(memo -> {
+            System.out.println(memo);
+
+        });
+    }
+
+    @Test
+    void testQueryMethods(){ //쿼리 메소드
+        List<Memo> list=memoRepository.findByMnoBetweenOrderByMnoDesc(70L,80L);
+
+        for(Memo memo: list){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    void testQueryMethodWithPageable(){
+        Pageable pageable = PageRequest.of(0,10,Sort.by("mno").descending());
+        Page<Memo> result =  memoRepository.findByMnoBetween(10L,50L,pageable);
+
+        result.get().forEach(System.out::println);
+    }
+
+
 }
